e8a77474a269215ac2dcb023df46084409443c00
Cesar-Fex04 <a222200714@unison.mx>
2025-08-26 15:34:01 -0700

    Primer commit del proyecto Portafolio

---
+++ Portafolio/Servicios/ServicioUnico.cs
@@ -0,0 +1,10 @@
+﻿namespace Portafolio.Servicios
+{
+    public class ServicioUnico
+    {
+        public ServicioUnico() {
+            ObtenerGuid = Guid.NewGuid();
+        }
+        public Guid ObtenerGuid { get; private set; }
+    }
+ }
